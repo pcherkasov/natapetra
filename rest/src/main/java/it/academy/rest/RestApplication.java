c620b3d36ca596fa2e7963b340ee1f23edabac85
Nata Petra <nika.live.1@mail.ru>
2023-03-22 23:28:17 +0300

    Add liquibase migration

--- rest/src/main/java/it/academy/rest/RestApplication.java
+++ rest/src/main/java/it/academy/rest/RestApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"it.academy.repository","it.academy.rest.controller", "it.academy.configuration", "it.academy.service"})
@@ -12,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class RestApplication {
 
     public static void main(String[] args) {
+        System.setProperty("liquibase.secureParsing", System.getProperty("liquibase.secureParsing", "false"));
         SpringApplication.run(RestApplication.class, args);
     }
 
